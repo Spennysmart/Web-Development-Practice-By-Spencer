22cabe1031531c843d15f0a08964791c294c71d9
Spennysmart <127111068+Spennysmart@users.noreply.github.com>
2025-08-01 21:33:44 +0100

    Add files via upload

---
+++ learn1.js
@@ -0,0 +1,10 @@
+let firstName = "Spencer";
+const country = "Nigeria";
+let age = 23;
+let isLearning = true;
+
+console.log("Hello, my name is " + firstName);
+console.log("I am from " + country);
+console.log("I am " + age + " years old.");
+console.log("Am I learning JavaScript? " + isLearning);
+
